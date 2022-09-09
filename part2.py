0e60378f15d208b3b73da7579da0a48350affddb
JasonWongLY <lapyinbhscmc@gmail.com>
2022-09-09 16:51:57 +0800

    remove the reminder part of the array

--- part2.py
+++ part2.py
@@ -1,6 +1,17 @@
+from itertools import groupby
 def to_cumulative_delayed(str,num):
-    str.sort()
-    index=len(str)%num
-    str[:,num-index]
-    splitted=[i.split(',') for i in str]
-    #test
+    # str.sort()
+    # str[:,num-index]
+    str=[i.split(',') for i in str]
+    key_func=lambda x:x[0]
+    tick_func=lambda x:x[1]
+    x=[list(j) for i,j in groupby(str,tick_func)]
+    for i,j in enumerate(x):
+        j.sort()
+        idx=len(j)%num
+        x[i]=j[:len(j)-idx]
+        
+    print(x)
+
+test=['00:06,A,1,5.6','00:05,A,1,5.6','00:00,A,1,5.6','00:02,A,1,5.6','00:03,A,1,5.6','00:04,A,1,5.6','00:06,B,1,5.6','00:05,B,1,5.6','00:00,B,1,5.6','00:02,B,1,5.6','00:03,B,1,5.6','00:04,B,1,5.6']
+to_cumulative_delayed(test,5)
